e59e5a758b45e1393a8889c095e411489194b12d
zz-min <kjm77878@gmail.com>
2022-05-24 04:51:23 +0900

    .

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,15 +1,86 @@
-import React from "react";
+import React,{Component} from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-function Main() {
-  const navigate = useNavigate();
+export class Main extends Component {
+  state={
+    isLoading:true,
+    airlineList:[],
+    airportList:[]
+    };
 
-  if (localStorage.getItem("loginValidity") === "false") {
-    alert("Login을 먼저 해주세요");
-    navigate("/login");
+  getAirlineList= async ()=>{
+    const r=await axios.get("https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getAirmanList?serviceKey=fhy41313p5usuDFdab0hFuBpAm0r2ByZwbHyFOFtRnOVjvXRYSJVdLJ64xx7FFryhq3fk9%2B6fuiLaBaoF9EZqg%3D%3D&_type=json");
+    this.setState({airlineList:r.data.response.body.items.item,isLoading:false});
+  }
+    
+
+  getAirportList= async ()=>{
+    const r=await axios.get("https://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getArprtList?serviceKey=fhy41313p5usuDFdab0hFuBpAm0r2ByZwbHyFOFtRnOVjvXRYSJVdLJ64xx7FFryhq3fk9%2B6fuiLaBaoF9EZqg%3D%3D&_type=json");
+    this.setState({airportList:r.data.response.body.items.item,isLoading:false});
   }
 
-  return <div>Main</div>;
+
+  componentDidMount(){
+    this.getAirlineList();
+    this.getAirportList();
+  }
+
+  render(){
+    const {isLoading,airportList,airlineList}=this.state;
+
+    if (localStorage.getItem("loginValidity") === "false") {
+      alert("Login을 먼저 해주세요");
+      //navigate("/login");
+    }
+    
+    return (
+      <>
+      <section className="container">
+        {isLoading ? 
+        (
+          <div className="loader__text">
+            <span>Loading...</span>
+          </div>
+        ) 
+        : 
+        (
+          <form id="serchFlightForm">
+           <label>항공사 : </label> 
+           <select id="checkedAirline" className="checkedserchFlight">
+            <option value="none" >=== (필수) === </option>
+            {airlineList.map((airline) => {
+              return (
+                <option value={airline.airlineNm} key={airline.airlineId}>{airline.airlineNm}</option>
+              );
+            })}
+           </select>
+           <label>출발공항 : </label> 
+           <select id="checkedOriginAirport" className="checkedserchFlight" required="required">
+            <option value="none" >=== (필수) === </option>
+            {airportList.map((airport) => {
+              return (
+                <option value={airport.airportNm} key={airport.airportId}>{airport.airportNm}</option>
+              );
+            })}
+           </select>
+           <label>도착공항 : </label> 
+           <select id="checkedDestAirport" className="checkedserchFlight" required="required">
+            <option value="none" >=== (필수) === </option>
+            {airportList.map((airport) => {
+              return (
+                <option value={airport.airportNm} key={airport.airportId}>{airport.airportNm}</option>
+              );
+            })}
+           </select>
+          </form>    
+        )
+        }
+      </section>    
+      </>
+    );
+  }
+  
 }
 
 export default Main;
